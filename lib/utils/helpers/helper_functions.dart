6c617b2e36325891a49167bb1fb63f5ed0a22aaf
itiswd <itiswdev97@gmail.com>
2024-09-17 15:58:59 +0300

    Add utility helper functions for common tasks.

---
+++ lib/utils/helpers/helper_functions.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+class THelperFunctions {
+  static Color? getColor(String color) {
+    switch (color) {
+      case 'Green':
+        return Colors.green;
+      case 'Red':
+        return Colors.red;
+      case 'Blue':
+        return Colors.blue;
+      case 'Pink':
+        return Colors.pink;
+      case 'Grey':
+        return Colors.grey;
+      case 'Purple':
+        return Colors.purple;
+      case 'Black':
+        return Colors.black;
+      case 'White':
+        return Colors.white;
+      case 'Yellow':
+        return Colors.yellow;
+      case 'Orange':
+        return Colors.orange;
+      case 'Teal':
+        return Colors.teal;
+      case 'Brown':
+        return Colors.brown;
+      case 'Indigo':
+        return Colors.indigo;
+    }
+    return null;
+  }
+
+  static void showSnackBar(String message) {
+    ScaffoldMessenger.of(Get.context!).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  static void showAlert(String title, String message) {
+    showDialog(
+      context: Get.context!,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  static void navigateToScreen(BuildContext context, Widget screen) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => screen,
+      ),
+    );
+  }
+
+  static String truncateText(String text, int maxlength) {
+    if (text.length < maxlength) {
+      return text;
+    } else {
+      return '${text.substring(0, maxlength)}...';
+    }
+  }
+
+  static bool isDarkMode(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.dark;
+  }
+
+  static Size screenSize() {
+    return MediaQuery.of(Get.context!).size;
+  }
+
+  static double screenHeight() {
+    return screenSize().height;
+  }
+
+  static double screenWidth() {
+    return screenSize().width;
+  }
+
+  static String getFormatDate(DateTime date, {String format = 'dd/MMMM/yyyy'}) {
+    return DateFormat(format).format(date);
+  }
+
+  static List<T> removeDuplicates<T>(List<T> list) {
+    return list.toSet().toList();
+  }
+
+  static List<Widget> wrapWidgets(List<Widget> widgets, int rowSize) {
+    final wraptList = <Widget>[];
+    for (int i = 0; i < widgets.length; i += rowSize) {
+      wraptList.add(
+        Row(
+          children: widgets.sublist(i, i + rowSize),
+        ),
+      );
+    }
+    return wraptList;
+  }
+}
