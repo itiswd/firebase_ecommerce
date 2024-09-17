c770a5ebf93e8230696a5646e589a617227b0155
itiswd <itiswdev97@gmail.com>
2024-09-17 18:57:25 +0300

    Update App Assets and Dependencies
    - Added new onboarding images to the assets directory.
    - Updated splash screen images for various screen densities and themes.
    - Modified the iOS LaunchScreen.storyboard to reflect new image dimensions.
    - Added the `smooth_page_indicator` dependency to `pubspec.yaml`.
    - Updated `pubspec.lock` to include the new dependency.
    - Modified `lib/app.dart` to include a placeholder `Scaffold` widget in the `MyApp` class.

--- lib/app.dart
+++ lib/app.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
+      home: const Scaffold(),
     );
   }
 }
