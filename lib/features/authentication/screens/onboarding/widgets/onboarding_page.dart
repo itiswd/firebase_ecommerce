f958ffe938530d9745302dd2cc784768442e007b
itiswd <itiswdev97@gmail.com>
2024-09-18 00:05:20 +0300

    Update Onboarding Images and UI Layout
    Updated the onboarding images and modified the UI layout for the onboarding pages. The new images are now located in `assets/images/onboarding/` with filenames `onboarding_21.png`, `onboarding_22.png`, and `onboarding_23.png`. The corresponding image paths in `TImages` class have been updated to reflect these changes.
    
    Additionally, the UI layout for the `OnBoardingPage` widget has been refactored to center the content and adjust the image size. The `OnBoardingSkip` widget has also been updated to dynamically adjust text color based on the theme mode and increase the font size for better readability.

--- lib/features/authentication/screens/onboarding/widgets/onboarding_page.dart
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_page.dart
@@ -16,33 +16,29 @@ class OnBoardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset(
-          width: THelperFunctions.screenWidth(),
-          height: THelperFunctions.screenHeight() * 0.6,
-          fit: BoxFit.fill,
-          image,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(TSizes.defaultSpacing),
-          child: Column(
-            children: [
-              Text(
-                title,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              const SizedBox(height: TSizes.spaceBtwItems),
-              Text(
-                subTitle,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.all(TSizes.defaultSpacing),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            width: THelperFunctions.screenWidth() * 0.65,
+            image,
           ),
-        ),
-      ],
+          const SizedBox(height: TSizes.spaceBtwSections),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const SizedBox(height: TSizes.spaceBtwItems),
+          Text(
+            subTitle,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/features/authentication/screens/onboarding/widgets/onboarding_skip.dart
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_skip.dart
@@ -2,6 +2,7 @@ import 'package:firebase_ecommerce/features/authentication/controllers/onboardin
 import 'package:firebase_ecommerce/utils/constants/colors.dart';
 import 'package:firebase_ecommerce/utils/constants/sizes.dart';
 import 'package:firebase_ecommerce/utils/device/device_utility.dart';
+import 'package:firebase_ecommerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class OnBoardingSkip extends StatelessWidget {
@@ -11,6 +12,7 @@ class OnBoardingSkip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
     return Positioned(
       top: TDeviceUtils.getAppBarHeight(),
       right: TSizes.defaultSpacing,
@@ -19,8 +21,9 @@ class OnBoardingSkip extends StatelessWidget {
         child: Text(
           'Skip',
           style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                color: TColors.primary,
+                color: dark ? TColors.primary : TColors.black,
                 fontWeight: FontWeight.bold,
+                fontSize: 18.0,
               ),
         ),
       ),
